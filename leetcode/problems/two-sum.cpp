10640f83bed17c21f4bb1741d4a8d282b1d8fe3c
Mohamed Gad <mohamedgad149@gmail.com>
2021-12-05 17:38:58 +0200

    initial

---
+++ leetcode/problems/two-sum.cpp
@@ -0,0 +1,26 @@
+#include <vector> 
+using namespace std; 
+
+class Solution {
+public:
+
+    vector<int> twoSum(vector<int>& arr, int givenNum) {
+        vector<int> result;
+        for (int i = 0; i < arr.size() - 1; i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if(arr[i] + arr[j] == givenNum) {
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+                }
+            }
+        }
+    return result;
+    }
+};
+
+int main() {
+    
+    
+    return 0; 
+}
