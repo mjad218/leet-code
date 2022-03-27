55544bd83846d33c65d9f24aa47735f89869e8f2
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-27 23:24:16 +0200

    phrase palindrome

---
+++ leetcode/problems/easy/palindrome/phrase-palindrome.cpp
@@ -0,0 +1,28 @@
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    bool isPal(string& s) {
+        int size = s.length();
+        for (int i = 0; i < size / 2; i++) {
+            if (s[i] != s[size - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isPalindrome(string s) {
+        string str = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (iswalnum(s[i])) {
+                char c = s[i];
+                if (isupper(c)) {
+                    c = tolower(c);
+                }
+                str += c;
+            }
+        }
+        return isPal(str);
+    }
+};
