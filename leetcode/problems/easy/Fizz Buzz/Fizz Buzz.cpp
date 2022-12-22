aaf8216d984ccae2bd16fbeceedaa0e50a7aadff
Mohamed Gad <59421219+mjad218@users.noreply.github.com>
2022-12-22 23:17:44 +0200

    Create   Fizz Buzz.cpp

---
+++ leetcode/problems/easy/Fizz Buzz/Fizz Buzz.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+               result.push_back("FizzBuzz"); 
+                continue;
+            }
+            if(i % 5 == 0) {
+               result.push_back("Buzz"); 
+                continue;
+
+            }
+            if(i % 3 == 0 ) {
+               result.push_back("Fizz");
+                continue;
+
+            }
+           result.push_back(to_string(i)); 
+
+        }
+        return result;
+    }
+};
